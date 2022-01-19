c7963fdd7eded361ec7caff1490c2b0f372d7c81
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-01-18 19:46:42 -0600

    Add dependencies and add App component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Cryptoverse</h1>
+    </div>
+  )
+};
+
+export default App;

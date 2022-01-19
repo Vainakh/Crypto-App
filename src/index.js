9377dccc443ff62fdf791364a90d8d4ce93520ac
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-01-18 22:52:05 -0600

    Add new components and set up routes. Use antd to style index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <Router>
-    <App />
+     <App />
   </Router>,
   document.getElementById('root')
 );

0e0596f17715efdd62e2ac071f196fb2ebf42f93
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-01-19 19:01:12 -0600

    Use redux tool kit to fetch data from rapid api endpoints. Render stats to the homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,9 +3,17 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
 const { Title } = Typography;
 
 const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+  const globalStats = data?.data?.stats;
+  console.log(globalStats);
+
+  if (isFetching) return 'Loading...';
+
   return (
     <>
       <Title level={2} className='heading'>Global Crypto Stats</Title>
@@ -13,31 +21,31 @@ const Homepage = () => {
         <Col span={12}>
           <Statistic
             title='Total Cryptocurrencies'
-            value='5'
+            value={globalStats.total}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title='Total Exchanges'
-            value='5'
+            value={millify(globalStats.totalExchanges)}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title='Total Market Cap'
-            value='5'
+            value={'$ ' + millify(globalStats.totalMarketCap)}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title='Total 24 Volume'
-            value='5'
+            value={millify(globalStats.total24hVolume)}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title='Total Markets'
-            value='5'
+            value={globalStats.totalMarkets}
           />
         </Col>
       </Row>

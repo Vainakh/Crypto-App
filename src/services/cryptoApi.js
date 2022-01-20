d03ba0e47e6f17b9353ec5c87b7ded02a69197ee
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2022-01-19 18:20:21 -0600

    Install redux. Create store file. Set provider wrapping App.

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+// },
+// headers: {
+//   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//   'x-rapidapi-key': 'e2a5051cfbmsh9c9d47956724f15p1bc602jsn9f9583241572'
+// }
